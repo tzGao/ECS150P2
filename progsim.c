6a43ac15138f87eab0a37384e45b0ea992e3dc4a
Knhwong <47486931+Knhwong@users.noreply.github.com>
2022-05-28 01:36:24 -0700

    Update progsim.c

--- progsim.c
+++ progsim.c
@@ -1,8 +1,7 @@
-//Just Parser right now.
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "queue.h"
 
 struct Process {
     char name[256];
@@ -10,28 +9,43 @@ struct Process {
     float prob;
 };
 
-void readFile(char* fileName, FILE* fp){
+struct Process* makeProcess(char* name, int time, float prob) {
+    struct Process* p = malloc(sizeof(struct Process));
+    strcpy(p->name, name);
+    p->time = time;
+    p->prob = prob;
+    return p;
+}
+
+
+struct queue readFile(char* fileName, FILE* fp){
     fp = fopen(fileName, "r");
+
+    queue_t q = queue_create();
+    
+    
     char name[256];
     int time;
     float prob;
-    char curr = EOF;
+    int curr = EOF;
     do
     {   
         curr = fscanf(fp, "%s %d %f", name, &time, &prob);
-        //Pointer Malloc so we enqueue in.
-        struct Process* p = malloc(sizeof(struct Process));
-        strcpy(p->name, name);
-        p->time = time;
-        p->prob = prob;
-
-        printf("%s %d %f \n", p->name, p->time, p->prob);
+        if (curr != -1) {
+            struct Process* p = makeProcess(name, time, prob);
+            printf("%s %d %f \n", p->name, p->time, p->prob);
+            queue_enqueue(q, p);
+        }
     } while (curr != EOF);
+
+
+    //printf("%d \n", queue_length(q));
 
     fclose(fp);
 }
 
 int main(int argc, char* argv[]){
     FILE* fp;
-    readFile(argv[1], fp);
+    struct queue q = readFile(argv[1], fp);
+
 }
